5469e04612c0a66cde4b6ffd401d4658cf107e22
Ayush Kumar Singh <54079897+developerAkX@users.noreply.github.com>
2022-10-30 13:41:30 +0530

    Added: Email Button Hover

--- src/common/CommonElements.js
+++ src/common/CommonElements.js
@@ -1,24 +1,27 @@
 import styled from "@emotion/styled";
 
-
-
 export const Button = styled.button`
   border-radius: 12px;
   background: #df581e;
   padding: 10px 22px;
   color: #fff;
   outline: none;
-  font-weight: 700;
+  font-weight: 600;
   border: none;
   cursor: pointer;
   box-shadow: inset 0px 4px 4px rgba(255, 255, 255, 0.25);
   transition: all 0.2s ease-in-out;
   text-decoration: none;
-
+  font-size: 17px;
   &:hover {
     transition: all 0.2s ease-in-out;
-    background: #fff;
-    color: #56402a;
+    /* background: #fff; */
+    /* color: #56402a; */
+    background: #bf510b;
+  }
+  @media (max-width: 510px) {
+    font-size: 18px;
+    font-weight: 500;
   }
 `;
 
